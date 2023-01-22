b4afeed9e8954d952314ba72bfc87a916c329706
darkdread <sakatagintoki9001@gmail.com>
2023-01-23 02:05:38 +0800

    add treesitter

--- lua/darkdread/packer.lua
+++ lua/darkdread/packer.lua
@@ -17,6 +17,10 @@ require('packer').startup(function(use)
     }
 
     use {
+        'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}
+    }
+
+    use {
         'nvim-tree/nvim-tree.lua',
         requires = {
             'nvim-tree/nvim-web-devicons', -- optional, for file icons
